fdc5732f74d61a8a4738749496dedf3e086ff466
Tyler Shin <shincode@icloud.com>
2020-04-01 16:17:38 +0900

    fix FOUC problem

--- src/client/helpers/loadWebFont.tsx
+++ src/client/helpers/loadWebFont.tsx
@@ -1,10 +1,11 @@
-export async function loadWebFont() {
-  const WebFont = await import('webfontloader');
+import WebFont from 'webfontloader';
 
+export function loadWebFont() {
   WebFont.load({
     custom: {
       families: ['Roboto'],
       urls: ['https://assets.pluto.network/font/roboto-self.css'],
     },
+    timeout: 3000,
   });
 }
